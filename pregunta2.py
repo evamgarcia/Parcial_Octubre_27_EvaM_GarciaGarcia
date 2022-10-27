1835ec6d5e1bdf0ac22df6b5e98e3ebf1b6a8225
evamgarcia <emgarcia2911@gmail.com>
2022-10-27 20:21:12 +0200

    ejericico 1

--- pregunta2.py
+++ pregunta2.py
@@ -1,9 +1,21 @@
-lista = [18, 50, 210, 80, 145, 33, 70, 30]
-lista2 = []
-for i in lista:
-    if i % 10 == 0 and i < 200:
-        lista2.append(i)
-print(lista2)
+
+
+def multiplos(lista):
+    lista2 = []
+    for i in lista:
+        if i % 10 == 0 and i < 200:
+            lista2.append(i)
+            return lista2
         
+def no_masde_300(lista):
+    for i in lista:
+        if i > 300:
+            break 
+        else:
+            return(lista)
 
 
+lista = [18, 50, 210, 80, 145, 333, 70, 30]
+
+m = multiplos(lista)
+print(m)
