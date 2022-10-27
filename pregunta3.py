9486ec7dfb8f53afc41e4aef9ec94b3410cec5d4
evamgarcia <emgarcia2911@gmail.com>
2022-10-27 19:48:21 +0200

    ejericcio 3

--- pregunta3.py
+++ pregunta3.py
@@ -1,17 +1,29 @@
+
+
+
 class Alumno():
     def __init__(self, nombre, nota):
         self.nombre = nombre 
         self.nota = nota 
 
-    def __str__(self):
-        return "El alumno se llama {}, y tiene {} de nota".format(self.nombre, self.nota)
+def mostrar (self):
+    print("Nombre: ", self.nombre)
+    print("Nota: ", self.nota)
+    print("El alumno se ha creado con exito")
 
-def calificacion(nota):
-    if nota >= 5:
-        print("el alumno esta aprobrado ")
+
+def calificacion(self):
+    if self.nota >= 5:
+        print("el alumno {} esta aprobrado ").format(self.nombre)
     else:
         print("el alumno esta suspendido")
 
 a = Alumno("marcos", 10)
 b = Alumno("fernando", 4)
 i = Alumno("paula", 8)
+
+p = calificacion(a)
+r = calificacion(i)
+print(p,i)
+
+
